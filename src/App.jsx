5a5545812d113185c31a5729c6c64444ac6c0424
Shivani-tech01 <shivanirsingh.01@gmail.com>
2026-04-16 11:18:43 +0530

    added country detail page and useCountry hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import CountryPage from './pages/CountryPage'
 
 function App() {
   return (
-    <BrowserRouter>
-      {/* Header always visible */}
-      <Header />
-
-      <main>
-        <Routes>
-          {/* Home Page */}
-          <Route path="/" element={<Home />} />
-
-          {/* Country Page (placeholder for now) */}
-          <Route path="/country/:name" element={<div>Country Page</div>} />
-
-          {/* Favourites Page (placeholder for now) */}
-          <Route path="/favourites" element={<div>Favourites Page</div>} />
-
-          {/* 404 Page */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/country/:code" element={<CountryPage />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
