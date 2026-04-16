2f9d9b24bbaa39694aafe331e853a0f7be8d4121
Shivani-tech01 <shivanirsingh.01@gmail.com>
2026-04-16 10:39:45 +0530

    feat: connect RestCountries API with search and country cards

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,16 @@
-import { Link } from "react-router-dom";
-import "../styles/App.css";
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <header className="header">
-      {/* App Name */}
-      <Link to="/" className="header__brand">
-        CountryPeek
-      </Link>
+      <div className="header__logo">CountryPeek</div>
 
-      {/* Navigation */}
       <nav className="header__nav">
         <Link to="/">Home</Link>
         <Link to="/favourites">Favourites</Link>
       </nav>
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
